a2c09f4aab06bae045780c24087a31d41a6aa210
Juan Pablo <jpmunizaga19@gmail.com>
2023-04-20 12:55:18 -0300

    ejercicio 2 armado

---
+++ Ejercicio2/claseViajero.py
@@ -0,0 +1,28 @@
+class ViajeroFrecuente:
+    __num_viajero = ""
+    __dni = ""
+    __nombre = ""
+    __apellido = ""
+    __millas_acumuladas = ""
+    
+    def __init__(self, num_viajero, dni, nombre, apellido, millas_acumuladas):
+        self.num_viajero = num_viajero
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        self.millas_acumuladas = millas_acumuladas
+
+    def cantidad_total_de_millas(self):
+        return self.millas_acumuladas
+
+    def acumular_millas(self, millas_recorridas):
+        self.millas_acumuladas += millas_recorridas
+        return self.millas_acumuladas
+
+    def canjear_millas(self, millas_a_canjear):
+        if millas_a_canjear <= self.millas_acumuladas:
+            self.millas_acumuladas -= millas_a_canjear
+            return self.millas_acumuladas
+        else:
+            print("Error: No se pueden canjear más millas de las acumuladas.")
+            return self.millas_acumuladas
---
+++ Ejercicio2/main.py
@@ -0,0 +1,47 @@
+import csv
+from claseViajero import ViajeroFrecuente
+
+viajeros = []
+
+with open("viajeros.csv", 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        num_viajero, dni, nombre, apellido, millas_acumuladas = row
+        viajero = ViajeroFrecuente(num_viajero, dni, nombre, apellido, int(millas_acumuladas))
+        viajeros.append(viajero)
+
+def menu():
+    print("Opciones:")
+    print("a - Consultar Cantidad de Millas")
+    print("b - Acumular Millas")
+    print("c - Canjear Millas")
+    print("q - Salir")
+
+def buscar_viajero(num_viajero):
+    for viajero in viajeros:
+        if viajero.num_viajero == num_viajero:
+            return viajero
+    return None
+
+while True:
+    menu()
+    opcion = input("Ingrese una opción: ")
+
+    if opcion == 'q':
+        break
+
+    num_viajero = input("Ingrese el número de viajero frecuente: ")
+    viajero = buscar_viajero(num_viajero)
+
+    if viajero is None:
+        print("No se encontró el viajero frecuente.")
+        continue
+
+    if opcion == 'a':
+        print("Cantidad de millas acumuladas:", viajero.cantidad_total_de_millas())
+    elif opcion == 'b':
+        millas_recorridas = int(input("Ingrese la cantidad de millas recorridas: "))
+        print("Nuevo total de millas acumuladas:", viajero.acumular_millas(millas_recorridas))
+    elif opcion == 'c':
+        millas_a_canjear = int(input("Ingrese la cantidad de millas a canjear: "))
+        print("Nuevo total de millas acumuladas:", viajero.canjear_millas(millas_a_canjear))
